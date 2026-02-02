66e7e04481c3c13903bb7a9aae49900a69e65b96
Alberto <apenam@unamad.edu.pe>
2026-02-02 09:38:21 -0500

    datos para que se ejecute con ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'srqd-backend',
+      script: 'dist/src/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3002,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3002,
+      },
+    },
+  ],
+};
